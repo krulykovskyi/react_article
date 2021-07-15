f7ae92ac66a738fd311f8a91bffdd564a95b4e26
Artem Krulykovskyi <krulikovskiy.a@gmail.com>
2021-07-15 23:59:55 +0300

    Updates

---
+++ precache-manifest.cfb23974c71e1442fe28468fb78aa392.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "051e70a3ca21a9d6e09a115e705350f6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d740eeb432a1902ee679",
+    "url": "./static/js/2.ffcb5bc3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ffcb5bc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9c5f29af0c4df79879d",
+    "url": "./static/js/main.9719118b.chunk.js"
+  },
+  {
+    "revision": "994f72ec58672ee8392d",
+    "url": "./static/js/runtime-main.a5fa282b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.cfb23974c71e1442fe28468fb78aa392.js"
 );
 
 self.addEventListener('message', (event) => {
